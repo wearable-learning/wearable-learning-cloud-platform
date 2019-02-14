bbf0d89044dccd21fe14231977f535879e4c261e
mmicciolo <mmicciolo@wpi.edu>
2019-02-13 22:19:19 -0500

    Manual merge of ankits changes.

---
+++ WLCPWebApp/src/main/java/wlcp/webapp/spring/controller/LoginController.java
@@ -0,0 +1,38 @@
+package wlcp.webapp.spring.controller;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import wlcp.model.master.Username;
+import wlcp.webapp.dto.UserDto;
+
+@Controller
+@RequestMapping("/Controllers")
+public class LoginController {
+
+	@Inject
+	EntityManager entityManager;
+
+	@PostMapping(value="/userLogin")
+	@ResponseBody
+	public ResponseEntity<Boolean> login(@RequestBody UserDto user) { 
+		Username username = entityManager.find(Username.class, user.getUsername());
+		if(username!=null) {
+			
+			return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+		}
+		else {
+			
+			return new ResponseEntity<Boolean>(false, HttpStatus.OK);
+		}
+		
+	}
+}
