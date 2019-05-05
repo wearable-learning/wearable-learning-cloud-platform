95693ce782d8d9b98def8391a90ad9527aa80732
mmicciolo <mmicciolo@wpi.edu>
2019-05-05 18:45:59 -0400

    Quick Start Help Template.

--- WLCPFrontEnd/WebContent/wlcpfrontend/controllers/GameEditor.controller.js
+++ WLCPFrontEnd/WebContent/wlcpfrontend/controllers/GameEditor.controller.js
@@ -661,6 +661,16 @@ sap.ui.controller("wlcpfrontend.controllers.GameEditor", {
 		
 	},
 	
+	quickStartHelp : function() {
+		this.quickStartHelpDialog = sap.ui.xmlfragment("wlcpfrontend.fragments.GameEditor.QuickStartHelp", this);
+		this.quickStartHelpDialog.open();
+	},
+	
+	closeQuickStartHelp : function() {
+		this.quickStartHelpDialog.close();
+		this.quickStartHelpDialog.destroy();
+	},
+	
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
