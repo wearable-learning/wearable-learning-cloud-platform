6cba34e199746f839df8de71a6e1bbb2223e4fe1
mmicciolo <mmicciolo@wpi.edu>
2019-07-19 00:57:34 -0400

    Fixed bug with connection removal warning.

--- WLCPFrontEnd/WebContent/wlcpfrontend/controllers/GameEditor.controller.js
+++ WLCPFrontEnd/WebContent/wlcpfrontend/controllers/GameEditor.controller.js
@@ -183,7 +183,7 @@ sap.ui.controller("wlcpfrontend.controllers.GameEditor", {
 		if(oEvent.suspendedElementId == oEvent.targetId || typeof oEvent.suspendedElementId === "undefined") {
 			for(var i = 0; i < this.connectionList.length; i++) {
 				if(this.connectionList[i].connectionId == oEvent.id) {
-					if(this.connectionList[i].connectionTo.getActiveScopes().length > 0) {
+					if(this.connectionList[i].connectionFrom.getActiveScopes().length > 0) {
 						sap.m.MessageBox.confirm("By clicking OK, the validation engine will revalidate causing possible data loss in states and transitions below!", {title:"Confirm Connection Removal?", onClose : function (oEvent2) {
 							if(oEvent2 == sap.m.MessageBox.Action.OK) {
 								var connectionFrom = that.connectionList[i].connectionFrom.htmlId;
